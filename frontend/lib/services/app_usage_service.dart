53de3fc051af3af76a64c2badb542af5e2a4dc4e
Aditi Goyal <145578961+goyal02aditi@users.noreply.github.com>
2025-08-31 20:49:00 +0530

    corrected user app usage data collection

--- frontend/lib/services/app_usage_service.dart
+++ frontend/lib/services/app_usage_service.dart
@@ -2,17 +2,29 @@ import 'package:flutter/services.dart';
 
 class AppUsageService {
   static Future<bool> hasUsagePermission() async {
-    return await _channel.invokeMethod('hasUsagePermission');
+ try {
+    final res = await _channel.invokeMethod('hasUsagePermission');
+    return res == true;
+  } catch (e) {
+    print('hasUsagePermission error: $e');
+    return false;
+  }
   }
   static const MethodChannel _channel = MethodChannel('app.usage.channel');
 
   static Future<List<Map<String, dynamic>>> getAppUsageStats(int start, int end) async {
-    final List<dynamic> result = await _channel.invokeMethod('getUsageStats', {
-      'start': start,
-      'end': end,
-    });
-    return result.cast<Map<String, dynamic>>();
+    try {
+      final List<dynamic> result = await _channel.invokeMethod('getUsageStats', {
+        'start': start,
+        'end': end,
+      });
+      return result.cast<Map<String, dynamic>>();
+    } catch (e) {
+      print('getAppUsageStats error: $e');
+      return [];
+    }
   }
+     
 
   static Future<void> openUsageSettings() async {
     await _channel.invokeMethod('openUsageSettings');
